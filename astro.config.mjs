3cd4d7e3a55ec59f92d10bd7ae9662bd3a211c68
DavidsBerz <davids.berz@gmail.com>
2025-09-23 11:35:38 -0600

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ import image from '@astrojs/image'
 export default defineConfig({
     devToolbar: { enabled: false },
     integrations: [tailwind(), image()],
+    site: "https://github.com/Davids-Berz",         // raíz del sitio
+  base: "/roofing",
+  trailingSlash: "ignore",
 });
